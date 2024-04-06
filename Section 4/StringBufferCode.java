a408ca92effadc9c3adeec412ba8b0cc267cdd3e
TitaniumPlayer492003 <avijeetsingh492003@gmail.com>
2024-04-06 23:39:45 +0530

    Pushing after a long time to sync

---
+++ Section 4/StringBufferCode.java
@@ -0,0 +1,10 @@
+// String Class in immutable
+// String Buffer class is mutable
+
+import java.util.*;
+public class StringBufferCode{
+    public static void main(String... avi) {
+        StringBuffer sb =new StringBuffer("Hello");
+        System.out.println(sb);
+    }
+}
